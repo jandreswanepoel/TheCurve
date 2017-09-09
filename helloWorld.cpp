0f7f94f7835be43aedc79b8ac91e60e2e7ada08d
Jandre Swanepoel <jandreswanie@gmail.com>
2017-09-09 22:42:00 +0200

    Ek dink -m is sit n message by

---
+++ helloWorld.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+void printYay();
+int main()
+{
+	for (int i=0;i<900;i++)
+	{
+		cout << i << endl;
+	}
+  cout << "Hello world"<< endl;
+  printYay();
+  return 0;
+  
+}
+
+void printYay()
+{
+	std::cout << "Yay" <<std::endl;
+ }
